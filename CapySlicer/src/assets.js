a21ff5e4817eb56a1d65f0a02fb16046879cf4ac
Jules OUTIN <jules.outin@ynov.com>
2026-01-16 13:40:53 +0100

    Mise en place du projet

---
+++ CapySlicer/src/assets.js
@@ -0,0 +1,23 @@
+export default {
+    // 'audio': {
+    //     score: {
+    //         key: 'sound',
+    //         args: ['assets/sound.mp3', 'assets/sound.m4a', 'assets/sound.ogg']
+    //     },
+    // },
+    // 'image': {
+    //     spikes: {
+    //         key: 'spikes',
+    //         args: ['assets/spikes.png']
+    //     },
+    // },
+    'spritesheet': {
+        tiles: {
+            key: 'tiles',
+            args: ['assets/capy1.png', {
+                frameWidth: 500,
+                frameHeight: 500,
+            }]
+        },
+    },
+};
